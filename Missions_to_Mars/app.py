095420dd826bf78a19ed570340bc5a87601c1059
marcuspttr <marcuspttr@gmail.com>
2021-10-28 07:21:34 -0700

    Got it to work. Still need to clean up template and finish pulling images.

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,37 +1,37 @@
+from os import makedirs
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import mission_to_mars
+import mars_scrape
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
+    mars_info = mongo.db.mars_info.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_data = mars_data)
-
+    return render_template("index.html", mars_info = mars_info)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars_data = mission_to_mars.scrape_info()
+    mars_data = mars_scrape.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.mars_info.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/", code = 302)
 
 
 if __name__ == "__main__":
--- Missions_to_Mars/mars_scrape.py
+++ Missions_to_Mars/mars_scrape.py
@@ -2,6 +2,7 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 
 def scrape_info():
     # Set up Splinter
@@ -11,7 +12,7 @@ def scrape_info():
     # Path to Mars website.
     url = "https://redplanetscience.com/"
     browser.visit(url)
-   
+    
     # Pause to let the page load.
     time.sleep(1)
 
@@ -21,33 +22,56 @@ def scrape_info():
 
     # Get the latest News Title
     # From inspect I found: <div class = "content_title">
-    news_title = soup.find('div', id = "content_title")
+    news_title = soup.find('div', class_= "content_title").text
 
     # Get the latest News Paragraph text
     # From inspect I found: <div class="article_teaser_body">
-    news_p = soup.find('div',id = "article_tear_body")
-   
+    news_p = soup.find('div', class_= "article_teaser_body").text
+
     # Path to Mars image page.
-    url2 = "https://spaceimages-mars.com/"
-    browser.visit(url2)
-   
+    url_2 = "https://spaceimages-mars.com/"
+    browser.visit(url_2)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
     # Slooooowwwwww down. What's the rush?
-    time.sleep(1)
+    time.sleep(1)  
 
     # Find the src for the featured Mars image.
     # The main image is the second one featured.
-    relative_image_path = soup.find_all('img')[1]["src"]
-    mars_img = url2 + relative_image_path
+    img_source = soup.find('img', {"class" : "headerimage fade-in"})["src"]
+
+    mars_img = url_2 + img_source
+
+    # Pathing to Mars facts page.
+    url_3 = "https://galaxyfacts-mars.com/"
+
+    # Using Pandas to scrape the tables.
+    tables = pd.read_html(url_3)
+
+    # Grabbing the first table.
+    mars_df = tables[0] 
+
+    # Table has a formatted header. Removing it to make the first row the header instead.
+    header = mars_df.iloc[0]
+    mars_df = mars_df[1:]
+    mars_df.columns = header
+
+    # Saving the dataframe to html.
+    mars_facts = mars_df.to_html()
 
     # Store data in a dictionary
-    mars_data = {
+    mars_info = {
         "mars_img": mars_img,
         "news_title": news_title,
-        "news_p": news_p
+        "news_p": news_p,
+        "mars_facts" : mars_facts
     }
 
     # Close the browser after scraping
     browser.quit()
 
     # Return results
-    return mars_data
+    return mars_info
